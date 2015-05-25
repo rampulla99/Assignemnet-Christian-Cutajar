a1c15a9451d6e4a24fb4bc16c70a293bc032d093
rampulla99 <christian_cutajar@msn.com>
2015-05-25 12:11:04 +0100

    School Assignment Update 1

---
+++ Assignment/Assets/Coding/Facebook.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Facebook : MonoBehaviour {
+
+	void OnMouseDown(){
+		Application.OpenURL("http://www.facebook.com/");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
